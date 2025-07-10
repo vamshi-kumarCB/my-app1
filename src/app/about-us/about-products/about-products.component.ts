807a98712958544ae8b15782d47e7aead3e72977
vamshi-kumarCB <vamshimanu97@gmail.com>
2025-07-10 15:39:31 +0530

    loadings started

---
+++ src/app/about-us/about-products/about-products.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-products',
+  templateUrl: './about-products.component.html',
+  styleUrls: ['./about-products.component.css']
+})
+export class AboutProductsComponent {
+
+}
