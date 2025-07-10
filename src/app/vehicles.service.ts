bf023a793f9f43db6215f2f77e5497522f3b9b6a
vamshi-kumarCB <vamshimanu97@gmail.com>
2025-07-10 14:07:44 +0530

    custom api completed

--- src/app/vehicles.service.ts
+++ src/app/vehicles.service.ts
@@ -10,7 +10,8 @@ export class VehiclesService {
   
 
   constructor(private _httpClient:HttpClient) { }
-  apiURL:string='https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction';
+  apiURL:string='assets/data.json'
+  // apiURL:string='https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction';
 
 getVehicles():Observable<any>{
   return this._httpClient.get(this.apiURL);
